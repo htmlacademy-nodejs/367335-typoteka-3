1f66ea0ca7b0c0345bcac33dd41de4106d8ff8e8
Андрей Ефимов <efiand@ya.ru>
2020-09-07 13:52:48 +0300

    Шаблонизация

--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -4,11 +4,11 @@ const {Router} = require(`express`);
 const myRouter = new Router();
 
 myRouter.get(`/`, (req, res) => {
-  res.send(`/my`);
+  res.render(`my`);
 });
 
 myRouter.get(`/comments`, (req, res) => {
-  res.send(`/my/comments`);
+  res.render(`comments`);
 });
 
 module.exports = myRouter;
