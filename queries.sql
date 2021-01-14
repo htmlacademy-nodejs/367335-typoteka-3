e006e3d6504ee95f602077fe3a8a6948a0eaadc2
Andrey Efimov <efiand@ya.ru>
2021-01-14 23:14:10 +0300

    Правка запросов

--- queries.sql
+++ queries.sql
@@ -15,28 +15,24 @@ SELECT id, title, COUNT(ac.article_id) AS articles
   GROUP BY id;
 
 --Получить список публикаций (идентификатор публикации, заголовок публикации, анонс публикации, дата публикации, имя и фамилия автора, контактный email, количество комментариев, наименование категорий). Сначала свежие публикации:
-SELECT ap.id, ap.title, announce, pub_date, author, email, comments_count, STRING_AGG(ct.title, ', ') AS categories_list
-  FROM (select a.id, a.title, announce, pub_date, CONCAT(first_name, ' ', last_name) AS author, email, COUNT(cm.id) AS comments_count FROM articles a
-    JOIN peoples p ON p.id = a.people_id
-    FULL JOIN comments cm ON a.id = cm.article_id
-    GROUP BY a.id, p.id
-  ) ap
-  LEFT JOIN articles_categories ac ON ac.article_id = ap.id
+SELECT a.id, a.title, announce, pub_date, CONCAT(first_name, ' ', last_name) AS author, email, COUNT(DISTINCT cm.id) AS comments_count, STRING_AGG(DISTINCT ct.title, ', ') as categories_list
+  FROM articles a
+  JOIN peoples p ON p.id = a.people_id
+  FULL JOIN comments cm ON a.id = cm.article_id
+  LEFT JOIN articles_categories ac ON ac.article_id = a.id
   LEFT JOIN categories ct ON ac.category_id = ct.id
-  GROUP BY ap.id, ap.title, announce, pub_date, author, email, comments_count
+  GROUP BY a.id, p.id
   ORDER BY pub_date DESC;
 
 --Получить полную информацию определённой публикации (идентификатор публикации, заголовок публикации, анонс, полный текст публикации, дата публикации, путь к изображению, имя и фамилия автора, контактный email, количество комментариев, наименование категорий);
-SELECT ap.id, ap.title, announce, full_text, pub_date, picture, author, email, comments_count, STRING_AGG(ct.title, ', ') AS categories_list
-  FROM (select a.id, a.title, announce, full_text, picture, pub_date, CONCAT(first_name, ' ', last_name) AS author, email, COUNT(cm.id) AS comments_count FROM articles a
-    JOIN peoples p ON p.id = a.people_id
-    FULL JOIN comments cm ON a.id = cm.article_id
-    WHERE a.id = 1
-    GROUP BY a.id, p.id
-  ) ap
-  LEFT JOIN articles_categories ac ON ac.article_id = ap.id
+SELECT a.id, a.title, announce, pub_date, CONCAT(first_name, ' ', last_name) AS author, email, COUNT(DISTINCT cm.id) AS comments_count, STRING_AGG(DISTINCT ct.title, ', ') as categories_list
+  FROM articles a
+  JOIN peoples p ON p.id = a.people_id
+  FULL JOIN comments cm ON a.id = cm.article_id
+  LEFT JOIN articles_categories ac ON ac.article_id = a.id
   LEFT JOIN categories ct ON ac.category_id = ct.id
-  GROUP BY ap.id, ap.title, announce, full_text, pub_date, picture, author, email, comments_count;
+  WHERE a.id = 1
+  GROUP BY a.id, p.id;
 
 --Получить список из 5 свежих комментариев (идентификатор комментария, идентификатор публикации, имя и фамилия автора, текст комментария):
 SELECT c.id, article_id, CONCAT(first_name, ' ', last_name) AS author, text
